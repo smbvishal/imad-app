0983493f358ddca8ecedb36f46bcc5eb369ad816
smbvishal <smbvishal1@gmail.com>
2017-08-26 15:03:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,11 @@
-console.log('Loaded!');
+// Counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-// Change the text of the main-text divider
-
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New Value';
-
-//Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight ()
+button.onclick = function()
 {
-    marginLeft = marginLeft +5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() 
-{
-   var interval = setInterval(moveRight,50);
+    
+    counter=counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
