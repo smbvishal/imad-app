59db51ac7a5f844b0b3cd9ea5afe7a45ab4f7d7e
smbvishal <smbvishal1@gmail.com>
2017-08-26 14:34:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,13 @@ element.innerHTML = 'New Value';
 
 //Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight ()
+{
+    marginLeft = marginLeft +1;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() 
 {
-  img.style.marginLeft = '100px';  
+   var interval = setInterval(moveRight,50);
 };
