57c3a45b0f7482559abdd9b0e0e5aef6f6a263c4
smbvishal <smbvishal1@gmail.com>
2017-08-26 13:14:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {  //handles specific urls
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {  //handles specific urls
-  res.send('Article one request handled here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {  //handles specific urls
-  res.send('Article two request handled here');
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res) {  //handles specific urls
-  res.send('Article three request handled here');
+ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
