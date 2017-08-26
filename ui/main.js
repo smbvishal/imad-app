9c0166c83c16d2bc28422dc31fd3d3f990cd77a7
smbvishal <smbvishal1@gmail.com>
2017-08-26 14:34:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight ()
 {
-    marginLeft = marginLeft +1;
+    marginLeft = marginLeft +5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() 
