4a4267c681f403e04ade80146550284f8cc4e67b
smbvishal <smbvishal1@gmail.com>
2017-08-26 15:10:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,29 @@
 // Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
-button.onclick = function()
-{
+button.onclick = function() {
     
-    counter=counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            //Take some action
+            if(request.status === 200 )
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://smbvishal1.imad.hasura-app.io/counter',true);
+    request.send(null);
+}
+
+
+
+
