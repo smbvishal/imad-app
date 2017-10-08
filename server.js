e572ccf01f649e747941f03c79851063bf8462d7
smbvishal <smbvishal1@gmail.com>
2017-10-08 16:28:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool=require('pg').Pool;
 
 var config = {
     user: 'smbvishal1',
-    database: 'myarticle',
+    database: 'smbvishal1',
     host : 'db.imad.hasura-app.io',
     port: '5432',
     password:process.env.DB_PASSWORD
