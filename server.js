559cd0279ca8277bf10653bc5c3e784945f74814
smbvishal <smbvishal1@gmail.com>
2017-10-08 16:37:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,25 @@ app.get('/test-db',function(req,res)
     });
 });
 
-
+app.get('/articles/:articleName', function(req,res)
+{
+    pool.query("SELECT *FROM WHERE title = " +req.params.articleName, function (err, result)
+    {
+        if(err)
+        {
+            res.status(500).send(srr.toString());
+        }else {
+            if(result.rows.length === 0 )
+            {
+                res.status(404).send('Article not found');
+            }else
+            {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+});
 var counter=0;
 app.get('/counter', function (req, res) {  //handles specific urls
   counter=counter+1;
